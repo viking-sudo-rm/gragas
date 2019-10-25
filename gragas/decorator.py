b3d009c7db891700fb6dd51cefb27cbc45deb3cb
Will Merrill <william.merrill@yale.edu>
2019-10-25 00:18:46 -0700

    Fixed decorator logic.

--- gragas/decorator.py
+++ gragas/decorator.py
@@ -3,9 +3,16 @@ from types import FunctionType
 
 def decorator(callback):
     """Decorator indicating that an object is a decorator."""
+    name = getattr(callback, "__name__", callback)
     call = getattr(callback, "__call__", None)
-    if (not isinstance(callback, FunctionType) and
-            not isinstance(call, FunctionType)):
-        name = getattr(callback, "__name__", callback)
-        raise ValueError("Decorator %s is not callable." % name)
-    return callback
+    if not callable(call):
+        raise ValueError(f"Attribute __call__ of {name} is not callable.")
+    elif isinstance(callback, type):
+        if not isinstance(call, FunctionType):
+            raise ValueError(f"Class {name} has no __call__ method.")
+        else:
+            return callback
+    elif not isinstance(callback, FunctionType):
+        raise ValueError(f"{name} is not a class or function.")
+    else:
+        return callback
--- gragas/tests/decorator_test.py
+++ gragas/tests/decorator_test.py
@@ -6,24 +6,21 @@ from gragas.decorator import decorator
 class TestDecorator(TestCase):
 
     def test_fn_decorated(self):
-
         @decorator
-        def f():
-            pass
+        def f(fn):
+            return fn
 
-    def test_decorator_fails(self):
+    def test_decorate_noncallable(self):
         with self.assertRaises(ValueError):
             decorator(3)
 
     def test_class_decorated(self):
-
         @decorator
         class A:
             def __call__(self):
                 pass
 
     def test_class_decorated_fail(self):
-
         with self.assertRaises(ValueError):
             @decorator
             class B:
